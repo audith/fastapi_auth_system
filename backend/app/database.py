903b90cbe293c1291520def555022f4f8aa97681
Audith Adhikary <audith@example.com>
2026-03-26 15:22:32 +0600

    not use in ci/cd

--- backend/app/database.py
+++ backend/app/database.py
@@ -5,16 +5,14 @@ from dotenv import load_dotenv
 from sqlalchemy.exc import OperationalError
 
 load_dotenv()
-
-# 🔹 Use TEST_DATABASE_URL if set (for CI), else fallback to normal DATABASE_URL
-DATABASE_URL = os.getenv("TEST_DATABASE_URL") or f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST')}/{os.getenv('POSTGRES_DB')}"
+DATABASE_URL = f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST')}/{os.getenv('POSTGRES_DB')}"
 
 try:
     engin = create_engine(DATABASE_URL)
     with engin.connect() as conn:
-        print(f"✅ Database connected: {DATABASE_URL}")
+        print("✅ Database connected")
 except OperationalError as exc:
-    print("❌ Failed to connect", exc)
+    print("Failed to connect", exc)
 
 SessionLocal = sessionmaker(bind=engin)
 Base = declarative_base()
