2023d665383dc2426b496e768be1722980321741
Audith Adhikary <audith@example.com>
2026-03-26 15:02:17 +0600

    add url and change ym for test_db

--- backend/app/database.py
+++ backend/app/database.py
@@ -5,14 +5,16 @@ from dotenv import load_dotenv
 from sqlalchemy.exc import OperationalError
 
 load_dotenv()
-DATABASE_URL = f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST')}/{os.getenv('POSTGRES_DB')}"
+
+# 🔹 Use TEST_DATABASE_URL if set (for CI), else fallback to normal DATABASE_URL
+DATABASE_URL = os.getenv("TEST_DATABASE_URL") or f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST')}/{os.getenv('POSTGRES_DB')}"
 
 try:
     engin = create_engine(DATABASE_URL)
     with engin.connect() as conn:
-        print("✅ Database connected")
+        print(f"✅ Database connected: {DATABASE_URL}")
 except OperationalError as exc:
-    print("Failed to connect", exc)
+    print("❌ Failed to connect", exc)
 
 SessionLocal = sessionmaker(bind=engin)
 Base = declarative_base()
